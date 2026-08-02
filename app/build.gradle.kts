332738c57b882c890bd354cb6e6508613a739264
aleksandreev2 <sashahumortele3@gmail.com>
2026-08-02 21:17:12 +0400

    chore: start 0.3.0 alpha series

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dollarreader.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.2.0-alpha11"
+        versionCode = 12
+        versionName = "0.3.0-alpha01"
 
         vectorDrawables {
             useSupportLibrary = true
