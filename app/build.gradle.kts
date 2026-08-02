5dcceb19f54edf705ea8c187ae0ad1ff099ff7d3
aleksandreev2 <sashahumortele3@gmail.com>
2026-08-02 18:50:51 +0400

    chore: bump version to alpha06

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dollarreader.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.0-alpha05"
+        versionCode = 6
+        versionName = "0.2.0-alpha06"
 
         vectorDrawables {
             useSupportLibrary = true
